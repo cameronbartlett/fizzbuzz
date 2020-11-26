a96ff885955d2eb1b08f2b751265e17c85dfe469
Cameron Bartlett <cameron.bartlett@protonmail.com>
2020-11-26 22:06:56 +0000

    Add comments and argument check

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// Include required libraries
 const path = require('path');
 const { Liquid } = require('liquidjs');
 
+// Set up liquid templating engine
 var engine = new Liquid({
     root: path.resolve(__dirname, 'views/'),
     extname: '.liquid' 
 });
 
+// Set 'arg' if a value has been passed in
 const arg = process.argv[2];
 
+// Render FizzBuzz to the command line
 engine
-    .renderFile("fizzbuzz", {num: (arg)?arg:100})
+    .renderFile("fizzbuzz", {num: (arg && (arg === parseInt(arg, 10)))?arg:100})
     .then(console.log)
